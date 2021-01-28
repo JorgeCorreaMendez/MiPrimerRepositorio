7e25cb35135162b47f79cab559374b9f88de0122
Jorge Correa <jorgecorreamen@gmail.com>
2021-01-28 14:08:23 +0000

    Update primer.py
    Estoy cambiando el fichero

--- primer.py
+++ primer.py
@@ -1,2 +1,9 @@
 #Primer fichero de phytom
 print("hola")
+letra = input("dame una letra: ")
+
+print("lo que escribiste fue", letra)
+
+numero = float(input("dame un número: "))
+
+print("lo que escribiste fue", (numero))
